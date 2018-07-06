80dae3bfd49ce0a1243e8b5bf2aa0391d561d43b
Maksym Koshovyi <maximkoshevoi61@gmail.com>
2018-07-06 14:31:34 +0300

    Updated SkipLast method in LinqEx.cs

--- LinqEx.cs
+++ LinqEx.cs
@@ -2,22 +2,18 @@
 
 public static class LinqEx
 {
-	public static IEnumerable<T> SkipLast<T>(this IEnumerable<T> source, int count)
-	{
-		var enumerator = source.GetEnumerator();
-		var queue = new Queue<T>(count + 1);
+    public static IEnumerable<T> SkipLast<T>(this IEnumerable<T> source, int count)
+    {
+        var enumerator = source.GetEnumerator();
+        var queue = new Queue<T>(count + 1);
 
-		while (true)
-		{
-			if (!enumerator.MoveNext())
-			{
-				break;
-			}
-			queue.Enqueue(enumerator.Current);
-			if (queue.Count > count)
-			{
-				yield return queue.Dequeue();
-			}
-		}
-	}
+        while (enumerator.MoveNext())
+        {
+            queue.Enqueue(enumerator.Current);
+            if (queue.Count > count)
+            {
+                yield return queue.Dequeue();
+            }
+        }
+    }
 }
